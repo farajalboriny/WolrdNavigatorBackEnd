361829df7641da8c789e546e06800246c9403a1b
Faraj <farag_alborene@hotmail.com>
2020-08-12 10:19:48 +0300

    update jar2

--- src/main/java/com/worldnavigator/configurations/MapBuilder.java
+++ src/main/java/com/worldnavigator/configurations/MapBuilder.java
@@ -3,8 +3,9 @@ package com.worldnavigator.configurations;
 import com.auth.model.MapEntity;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.io.File;
+import java.io.BufferedInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class MapBuilder {
 
@@ -14,7 +15,7 @@ public class MapBuilder {
     GameMap gameMap = null;
     try {
       ObjectMapper objectMapper = new ObjectMapper();
-      gameMap = objectMapper.readValue(new File("map.json"), GameMap.class);
+      gameMap = objectMapper.readValue(readFile(), GameMap.class);
 
     } catch (IOException e) {
       System.out.println(e.getMessage());
@@ -25,5 +26,20 @@ public class MapBuilder {
     JsonConverter jsonConverter = new JsonConverter();
     mapEntity.setMap(jsonConverter.convertMapToJson(gameMap));
     return mapEntity;
+  }
+
+  private String readFile() throws IOException {
+    ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+    InputStream stream = classLoader.getResourceAsStream("map.json");
+    if (stream != null) {
+      BufferedInputStream buffer = new BufferedInputStream(stream);
+      StringBuilder builder = new StringBuilder();
+      int read;
+      while ((read = buffer.read()) != -1) {
+        builder.append((char) read);
+      }
+      return builder.toString();
+    }
+    throw new IOException("error while reading file");
   }
 }
